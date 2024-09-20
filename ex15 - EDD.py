6e153d0bcd257b5777b688b1a24972b0a7db5c99
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-09-20 07:28:47 -0300

    Atualizar o ex15 - EDD.py

--- ex15 - EDD.py
+++ ex15 - EDD.py
@@ -4,7 +4,6 @@ Escreva um algoritmo que receba um vetor ordenado e um número extra e insira es
 número na sua posição correta no vetor ordenado, deslocando os outros números, se
 necessário. Qual é a complexidade no melhor e no pior caso deste algoritmo?
 """
-import time
 
 def inserir_nmr(vetor,num):
     i = 0
@@ -12,19 +11,7 @@ def inserir_nmr(vetor,num):
         i += 1
     vetor.insert(i,num)
     return vetor
-
-def medir_tempo(vetor,num):
-    inicio = time.time()
-    vetor_ordenado = inserir_nmr(vetor,num)
-    fim = time.time()
-    tempo_execucao = inicio - fim
-    print(f"Tempo de execução: {tempo_execucao:.10f} segundos")
-    return vetor_ordenado
-
-vetor_teste = list(range(1, 1000001)) 
-numero_para_inserir = 0
-vetor_final = medir_tempo(vetor_teste, numero_para_inserir)
-
+
 """
 Neste caso
 Complexidade no melhor caso: O(1) 
