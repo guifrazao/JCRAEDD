0207e9784f56b37cc86f74a9e3095135c4b1220b
Guilherme Augusto Frazão Ribeiro <143296175+guifrazao@users.noreply.github.com>
2024-11-01 11:12:46 -0300

    Delete M3L directory

--- M3L/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from .mergeSort import mergeSort
-from .quickSort import quicksort
-from .pesquisaBRcontador import pesquisaBR_estruturado
-from .pesquisaLRcontador import pesquisaLR_estruturado
--- M3L/modules/pesquisaLRcontador.py
+++
@@ -1,9 +0,0 @@
-def pesquisaLR_estruturado(target, list):
-    position = 0
-    count = 1
-    while position < len(list):
-        if target == list[position]:
-            return position, count
-        position += 1
-        count += 1
-    return -1, count
