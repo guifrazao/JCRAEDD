377c7733a1980c7a6148db84f02f5fdacdbdea62
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2024-11-07 13:56:25 -0300

    Update ex06.py

--- ex06.py
+++ ex06.py
@@ -13,10 +13,11 @@ def buscar_horarios(destino, tabela_horarios):
 def main():
     tabela_horarios = {
         "São José dos Campos": [("08:00", "09:30"), ("12:00", "13:30"), ("16:00", "17:30")],
+        "Jacareí": [("08:30", "10:00"), ("12:30", "14:00"), ("16:30", "18:00")],
         "Campinas": [("07:00", "09:00"), ("11:00", "13:00"), ("15:00", "17:00")],
         "Taubaté": [("09:00", "10:30"), ("13:00", "14:30"), ("17:00", "18:30")],
-        "Aparecida": [("10:00", "12:00"), ("14:00", "16:00"), ("18:00", "20:00")]
-    }
+        "Aparecida": [("10:00", "12:00"), ("14:00", "16:00"), ("18:00", "20:00")]    
+    }  
 
     destino = input("Digite o destino: ")
     horarios = buscar_horarios(destino, tabela_horarios)
