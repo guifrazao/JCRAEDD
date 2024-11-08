5104138dae0f5559f1abc7e5ab721590db254599
Guilherme Augusto Frazão Ribeiro <143296175+guifrazao@users.noreply.github.com>
2024-11-08 08:03:00 -0300

    Update M3Lex01.py

--- M3L/M3Lex01.py
+++ M3L/M3Lex01.py
@@ -1,3 +1,10 @@
+"""
+1. Implemente um programa estruturado e recursivo para pesquisa linear. Faça uma
+função de busca chamada pesquisaLR que receba como parâmetro o valor a ser
+encontrado e a referência do vetor onde a busca será efetuada. A função retornará -1,
+caso não encontre o item, ou retornará o índice, caso o encontre.
+"""
+
 def pesquisaLR_estruturado(target, list):
     position = 0
     while position < len(list):
