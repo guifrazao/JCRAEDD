bbe0df481e93909e8be65c84ec1595303874fb9a
Guilherme Augusto Frazão Ribeiro <143296175+guifrazao@users.noreply.github.com>
2024-11-08 08:04:32 -0300

    Update M3Lex04.py

--- M3L/M3Lex04.py
+++ M3L/M3Lex04.py
@@ -1,3 +1,12 @@
+"""
+4. Faça um programa que cadastre n produtos. Para cada produto devem ser
+cadastrados os seguintes dados: código, descrição e preço. Use um método de
+ordenação e em seguida calcule e mostre quantas comparações devem ser feitas
+para encontrar um funcionário pelo código:
+a. Usando busca sequencial.
+b. Usando busca binária.
+"""
+
 from modules import pesquisaLR_estruturado
 from modules import pesquisaBR_estruturado
 from modules import quicksort
