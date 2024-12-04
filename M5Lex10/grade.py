fc99cb0300ceb278b076c1c43e7ae834a8bb0c55
Guilherme Augusto Frazão Ribeiro <143296175+guifrazao@users.noreply.github.com>
2024-12-04 14:29:18 -0300

    Delete M5Lex10 directory

--- M5Lex10/grade.py
+++
@@ -1,5 +0,0 @@
-class Grade:
-    def __init__(self, data, number, next=None):
-        self.data = data
-        self.number = number
-        self.next = next
--- M5Lex10/student.py
+++
@@ -1,8 +0,0 @@
-
-class Student:
-    def __init__(self, name, number, next = None):
-        self.name = name
-        self.number = number
-        self.next = next
-        
-    
